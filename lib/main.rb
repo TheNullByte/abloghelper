c638cb4a4fb1a0f888e2938cea30146131ae338f
TheNullByte <enzo2813@yahoo.com>
2012-09-09 23:02:28 -0400

    updated a lot of stuff

--- lib/main.rb
+++ lib/main.rb
@@ -1,5 +1,27 @@
 require 'xmlsimple'
+require 'net/http'
+require 'socksify'
+require 'open-uri'
 
-config = XmlSimple.xml_in('~/Downloads/proxyrss.xml', { 'KeyAttr' => 'name' })
+config = XmlSimple.xml_in(Net::HTTP.get_response(URI.parse('http://www.xroxy.com/proxyrss.xml')).body, { 'KeyAttr' => 'name' })
+x=''
+config['channel'][0]['item'].each{|c|
+                                    c['proxy'].each{|a| x += a['ip'][0] + ":" + a['port'][0] + "\n"}
+                                  }
 
-config['channel'][0]['item'][0]['proxy'].each{|a| puts a['ip'][0] + ":" + a['port'][0]}
+proxylist = x.split("\n")
+
+puts proxylist
+
+proxylist.each{|a|
+  begin
+        TCPSocket::socks_server = a.split(":")[0]
+        TCPSocket::socks_port = a.split(":")[1]
+        timeout(5) do
+        open('http://timeunderscrutiny.blogspot.com')
+        puts "Operation with #{a} completed."
+      end
+  rescue Timeout::Error
+        puts "timed out, trying next proxy"
+  end
+  }

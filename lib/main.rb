2f2e12c2a49e2a21ff9b158fe435171cdbc7fff8
TheNullByte <enzo2813@yahoo.com>
2012-09-10 01:40:34 -0400

    changed exception handling

--- lib/main.rb
+++ lib/main.rb
@@ -17,15 +17,16 @@ proxylist.each{|a|
   begin
         TCPSocket::socks_server = a.split(":")[0]
         TCPSocket::socks_port = a.split(":")[1]
+        puts "trying #{a} as #{a.split(":")[0]} with #{a.split(":")[1]}"
         timeout(20) do
-        open('http://timeunderscrutiny.blogspot.com')
+        open('http://timeunderscrutiny.blogspot.com', "Referer" => "http://luxury4play.com")
         puts "Operation with #{a} completed."
       end
-  rescue Timeout::Error
-        puts "timed out, trying next proxy"
-  rescue SOCKSError
-        puts "generic Socks error"
-  rescue Exception
-        puts "Generic Error"
+  rescue Timeout::Error => e
+        puts "timed out, trying next proxy (#{e})"
+  rescue SOCKSError => e
+        puts "generic Socks error (#{e})"
+  rescue Exception => e
+        puts "Generic Error (#{e})"
   end
   }
